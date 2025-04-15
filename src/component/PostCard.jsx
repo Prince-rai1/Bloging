6d63d8c48f244b41841635504c9bb436aba5e13f
Prince Rai <princeraia578@gmail.com>
2025-04-15 09:56:52 +0530

    Update PostCard.jsx

--- src/component/PostCard.jsx
+++ src/component/PostCard.jsx
@@ -9,7 +9,7 @@ function PostCard({$id, title, featuredimage}) {
         <div className='w-full bg-gray-100 rounded-xl p-4 hover:shadow-2xl transition duration-300 ease-in-out hover:bg-gray-300 mb-20'>
             <div className='w-full justify-center mb-4'>
                 <img src={AppwriteServices.getFileview(featuredimage)} alt={title}
-                className='rounded-xl' width={300} height={300}/>
+                className='rounded-xl h-[250px] w-full object-cover'/>
 
             </div>
             <h2
